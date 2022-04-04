2dc987dbcfa60945cdaf1c2baf6d083b564e2b68
Andrei Borza <andrei.borza@gmail.com>
2022-04-05 03:45:34 +0900

    Add secret reveal sound

--- src/Day of the Flame/src/Global/Global.gd
+++ src/Day of the Flame/src/Global/Global.gd
@@ -2,9 +2,13 @@ extends Node
 
 onready var player_dying_sound = $PlayerDyingSound
 onready var rat_dying_sound = $RatDyingSound
+onready var secret_reveal_sound = $SecretRevealSound
 
 func play_player_dying_sound():
 	player_dying_sound.play()
 
 func play_rat_dying_sound():
 	rat_dying_sound.play()
+
+func play_secret_reveal_sound():
+	secret_reveal_sound.play();
---
+++ src/Day of the Flame/src/HiddenWall/HiddenWall.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+export(int) var health = 1
+onready var scecret_tile = $SecretTile
+onready var reveal_timer = $RevealTimer
+
+
+func _on_Hurtbox_hit(damage):
+	print("secret got hit", health)
+	
+	if health == 0:
+		reveal_timer.start()
+		scecret_tile.queue_free()
+	else: 
+		health -= 1
+
+
+func _on_RevealTimer_timeout():
+	Global.play_secret_reveal_sound()
+	queue_free()
